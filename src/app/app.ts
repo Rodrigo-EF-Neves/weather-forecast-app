b972a161aa91b45e47c0242bd16b040d70dae879
Rodrigo Neves <rodrigo.emilio2004@gmail.com>
2026-02-08 18:47:02 -0300

    Criando barra de pesquisa

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component} from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [InputTextModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
